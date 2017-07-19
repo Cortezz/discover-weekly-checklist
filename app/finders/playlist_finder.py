22f96c005f8af9cc5726bce313a753329b754dae
Cortezz <jospcortez@gmail.com>
2017-07-20 00:04:58 +0100

    Add playlist finder

---
+++ app/finders/playlist_finder.py
@@ -0,0 +1,8 @@
+from app.models.playlist import Playlist
+
+
+class PlaylistFinder(object):
+
+    @classmethod
+    def get_last_playlist_by_user_id(cls, user_id):
+        return Playlist.query.filter_by(user_id=user_id).order_by(Playlist.date.desc()).first()
